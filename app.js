cf2c71977bca35fe6244224d96238a88f3e5cfab
joshi-durgesh <durgesh7joshi9@gmail.com>
2024-01-06 05:04:05 +0530

    pormises

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const button = document.querySelector("button");
 const output = document.querySelector("p");
 
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Done!");
+    }, duration);
+  });
+  return promise;
+};
+
 function trackUserHandler() {
   navigator.geolocation.getCurrentPosition(
     (posData) => {
-      setTimeout(() => {
-        console.log(posData);
-      }, 2000);
-      console.log(posData);
+      setTimer(2000).then((data) => {
+        console.log(data, posData);
+      });
     },
     (error) => {
       console.log(error);
     }
   );
-  setTimeout(() => {
+  setTimer(1000).then(() => {
     console.log("Timmer done!");
-  }, 0);
+  });
   console.log("getting position...");
 }
 
